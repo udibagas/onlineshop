3b8ce7a1c81705c5d6dabbbdcd4834f09b29b5ed
Bagas Udi Sahsangka <udibagas@gmail.com>
2015-11-26 10:14:35 +0700

    tampilan seller

--- resources/views/product/_images.blade.php
+++ resources/views/product/_images.blade.php
@@ -1,11 +1,2 @@
 <img src="/uploads/{{ $product->image }}" alt="{{ $product->name }}" class="img-thumbnail" />
 <br /><br />
-<div class="row">
-	@for($i=0;$i<=5;$i++)
-
-	<div class="col-md-4">
-		<img src="/uploads/{{ $product->image }}" alt="{{ $product->name }}" class="img-thumbnail" />
-	</div>
-
-	@endfor
-</div>
--- resources/views/product/_seller.blade.php
+++ resources/views/product/_seller.blade.php
@@ -2,21 +2,21 @@
 	<div class="panel-heading">
 		<h3 class="panel-title">Seller</h3>
 	</div>
-	<table class="table table-striped table-hover">
-		<tbody>
-			<tr><td colspan="2"><h3><a href="/store/{{ $product->store_id }}">{{ $product->store->name }}</a></h3></td></tr>
-			<tr><td>Email</td><td>: <a href="mailto:udibagas@gmail.com">udibagas@gmail.com</a></td></tr>
-			<tr><td>Phone</td><td>: 085776884114</td></tr>					
-			<tr><td>WA</td><td>: 085776884114</td></tr>					
-			<tr><td>BB</td><td>: ABCDF5</td></tr>					
-			<tr><td>Website</td><td>: <a href="http://www.udibagas.com" target="_blank">http://www.udibagas.com</a></td></tr>					
-			<tr><td>Member Since</td><td>: 9-Nov-2015</td></tr>					
-			<tr><td>Location</td><td>: Jakarta</td></tr>					
-			<tr><td>Verified</td><td>: <span class="label label-success">YES</span></td></tr>					
-			<tr><td colspan="2">
-				<a class="fa fa-facebook-official fa-2x text-success" href="#" target="_blank"></a>
-				<a class="fa fa-twitter-square fa-2x text-success" href="#" target="_blank"></a>
-			</td></tr>
-		</tbody>
-	</table>
+
+	<div class="panel-body">
+		<h3><a href="/store/{{ $product->store_id }}">{{ $product->store->name }}</a></h3><hr />
+		<div class="text-muted">
+			<span class="fa fa-home"></span> {{ $product->store->address }}<br />
+			<a href="/lokasi/{{ $product->store->lokasi->id }}"><span class="fa fa-map-marker"></span> {{ $product->store->lokasi->nama }}</a><br />
+			<span class="fa fa-phone"></span> {{ $product->store->phone }} | 
+			<span class="fa fa-mobile"></span> {{ $product->store->mobile }}<br />
+			<span class="fa fa-whatsapp"></span> {{ $product->store->wa }} | 
+			<span class="fa fa-bb">BBM</span> {{ $product->store->bb }}<br />
+			<span class="fa fa-envelope-o"></span> <a href="mailto:{{ $product->store->email }}">{{ $product->store->email }}</a><br />
+			<span class="fa fa-facebook"></span> <a href="{{ $product->store->facebook }}" target="_blank">{{ $product->store->facebook }}</a><br />
+			<span class="fa fa-twitter"></span> <a href="{{ $product->store->twitter }}" target="_blank">{{ $product->store->twitter }}</a><br />
+			<span class="fa fa-instagram"></span> <a href="{{ $product->store->instagram }}" target="_blank">{{ $product->store->instagram }}</a><br />
+			<span class="fa fa-globe"></span> <a href="{{ $product->store->web }}" target="_blank">{{ $product->store->web }}</a>
+		</div>
+	</div>
 </div>
